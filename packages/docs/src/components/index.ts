4f8485bba477077759179ce49abb0340435659d3
Ozgur Gunes <o.gunes@gmail.com>
2025-10-21 16:36:02 +0300

    update @chassis-ui/docs specifiers

--- packages/docs/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-// Common reusable components for documentation sites
-export { default as NavDocsMenu } from './NavDocsMenu.astro'
-export { default as TableOfContents } from './TableOfContents.astro'
-export { default as NavLink } from './NavLink.astro'
-export { default as ThemeToggler } from './ThemeToggler.astro'
-export { default as ResponsiveImage } from './ResponsiveImage.astro'
-export { default as AddedIn } from './shortcodes/AddedIn.astro'
-export { default as Callout } from './shortcodes/Callout.astro'
-export { default as CxTable } from './shortcodes/CxTable.astro'
--- packages/docs/src/libs/index.ts
+++
@@ -1,7 +0,0 @@
-// Shared libraries for documentation sites
-export * from './image'
-export * from './layout'
-export * from './rehype'
-export * from './remark'
-export * from './toc'
-export * from './utils'
